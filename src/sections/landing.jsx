d8176d002c9d847fdb0407d56c4e1b97cfd518bc
jockaplay <jocksonmatheus@gmail.com>
2024-04-21 14:55:23 -0300

    pc circle deleted

--- src/sections/landing.jsx
+++ src/sections/landing.jsx
@@ -1,6 +1,6 @@
 const Landing = () => {
     return (
-        <section className="before:z-0 before:translate-y-10 before:absolute before:w-[30rem] before:h-[30rem] before:rounded-full before:bg-stone-100 before:opacity-25 flex flex-col space-y-10 items-center px-10 md:px-20 md:py-[5rem] pt-20 pb-10 md:flex-row-reverse bg-stone-100" style={{'backgroundImage': 'url("https://github.com/jockaplay/landing-page/blob/dev/src/assets/subtle-prism.svg?raw=true")'}}>
+        <section className="md:before:z-0 md:before:translate-y-10 md:before:absolute md:before:w-[30rem] md:before:h-[30rem] md:before:rounded-full md:before:bg-stone-100 md:before:opacity-25 flex flex-col space-y-10 items-center px-10 md:px-20 md:py-[5rem] pt-20 pb-10 md:flex-row-reverse bg-stone-100" style={{'backgroundImage': 'url("https://github.com/jockaplay/landing-page/blob/dev/src/assets/subtle-prism.svg?raw=true")'}}>
             <div className="animate-fade-up animate-delay-300 md:flex md:w-6/12 md:justify-end w-6/12">
                 <img className="md:w-8/12 z-10" src="https://i.postimg.cc/SRtd55rp/jockson.png" alt="me" />
             </div>
