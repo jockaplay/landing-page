f74be5e70aa2a1069ca3bdebff109b38a895aaf4
jockaplay <jocksonmatheus@gmail.com>
2024-05-19 13:45:34 -0300

    add section

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -2,12 +2,35 @@ const About = () => {
     return (
         <section id="sobre">
             <div className="bg-purple-950 text-white px-10 md:px-20 space-y-20 pt-10">
+                <div className="flex flex-col needed">
+                    <h2 className="text-center text-3xl font-semibold border-b-2 pb-10">5 motivos para ter um site!</h2>
+                    <ul className="flex flex-col list-disc pt-10 md:px-20 md:py-10 xl:grid grid-cols-2 gap-10">
+                        <li className="flex flex-row gap-5">
+                            <img className="flex h-8" src="https://i.postimg.cc/nLNV6cYH/icons8-conta-verificada-96.webp"/><p><strong>Presença Online e Visibilidade:</strong> Um site serve como a &quot;vitrine&quot; online, permitindo que pessoas de todo o mundo descubram você, sua empresa ou sua organização. É essencial para estabelecer uma presença digital e aumentar a visibilidade.</p>
+                        </li>
+                        <li className="flex flex-row gap-5">
+                            <img className="flex h-8" src="https://i.postimg.cc/nLNV6cYH/icons8-conta-verificada-96.webp"/><p><strong>Credibilidade e Profissionalismo:</strong> Ter um site bem projetado e informativo ajuda a construir credibilidade e profissionalismo. As pessoas tendem a confiar mais em empresas e profissionais que têm um site do que naqueles que não têm.</p>
+                        </li>
+                        <li className="flex flex-row gap-5">
+                            <img className="flex h-8" src="https://i.postimg.cc/nLNV6cYH/icons8-conta-verificada-96.webp"/><p><strong>Marketing e Publicidade:</strong> Um site é uma ferramenta crucial para estratégias de marketing digital, incluindo SEO (otimização para mecanismos de busca), marketing de conteúdo e campanhas de mídia paga. Ele permite alcançar um público mais amplo e específico.</p>
+                        </li>
+                        <li className="flex flex-row gap-5">
+                            <img className="flex h-8" src="https://i.postimg.cc/nLNV6cYH/icons8-conta-verificada-96.webp"/><p><strong>Expansão de Negócios:</strong> Um site pode ajudar na expansão dos negócios, alcançando novos mercados e clientes em diferentes regiões geográficas que, de outra forma, seriam inacessíveis.</p>
+                        </li>
+                        <li className="flex flex-row col-span-2 gap-5">
+                            <img className="flex h-8" src="https://i.postimg.cc/nLNV6cYH/icons8-conta-verificada-96.webp"/><p><strong>Informação e Comunicação:</strong> Um site permite compartilhar informações detalhadas sobre produtos, serviços, eventos e outras atualizações importantes. Também facilita a comunicação com clientes, parceiros e o público em geral através de formulários de contato, blogs, newsletters e chats ao vivo.</p>
+                        </li>
+                    </ul>
+                    <div className="flex w-full justify-center pt-20 xl:pt-5 gl:pt-10">
+                        <a href="mailto:jocksonmatheus@gmail.com" className="transition ease-out duration-170 flex text-center bg-white text-black px-10 py-5 rounded hover:scale-110 hover:text-purple-700">Quero um site para mim!</a>
+                    </div>
+                </div>
                 <h2 className="text-center text-3xl font-semibold border-b-2 pb-10">Sobre</h2>
                 <div className="flex space-x-10 justify-between">
                     <p className="md:w-6/12 text-justify">Olá, <span className="font-bold">meu nome é Jocka</span>. Atualmente estudando na Universidade Federal de Alagoas, cursando ciência da computação, procuro oportunidades de adquirir experiência na área de programação e tecnologia.<br/><br/>
                     Sempre fui apaixonado por tecnologia e por volta de 2019 entrei no mundo da programação e achei incrível, desde então tenho me aperfeiçoado e me proficionalizado cada vez mais, já fiz alguns projetos comerciais na empresa que trabalhava e alguns projetos pessoais, mas ainda não entrei em um grande projeto, e espero conseguir por meio deste site, mais visibilidade para talvez encontrar estas oportunidades.</p>
                     <a href="https://jockaplay.itch.io/" target="_blank" className="hidden md:flex flex-col text-center justify-center space-y-5 border-2 rounded-lg border-dashed object-cover w-5/12 px-20">
-                        <img className="object-scale-down w-50 h-50" src="https://i.postimg.cc/MHGt9x1D/store.png" alt="" srcset="" />
+                        <img className="object-scale-down w-50 h-50" src="https://i.postimg.cc/7L9XQHHz/store.webp" alt=""/>
                         <p>Game store</p>
                     </a>
                 </div>
@@ -37,7 +60,7 @@ const About = () => {
                     </ul>
                 </div>
             </div>
-            <img className="w-full" src="https://i.postimg.cc/Pxy2bNvJ/wave.png" alt="" srcset="" />
+            <img className="w-full" src="https://i.postimg.cc/QdqQqvDN/wave-2.png"/>
         </section>
     )
 }
--- src/sections/landing.jsx
+++ src/sections/landing.jsx
@@ -1,17 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Landing = () => {
     return (
         <section className="md:before:z-0 md:before:translate-y-10 md:before:absolute md:before:w-[30rem] md:before:h-[30rem] md:before:rounded-full md:before:bg-stone-100 md:before:opacity-25 flex flex-col space-y-10 items-center px-10 md:px-20 md:py-[5rem] pt-20 pb-10 md:flex-row-reverse bg-stone-100" style={{'backgroundImage': 'url("https://github.com/jockaplay/landing-page/blob/dev/src/assets/subtle-prism.svg?raw=true")'}}>
             <div className="animate-fade-up animate-delay-300 md:flex md:w-6/12 md:justify-end w-6/12">
-                <img className="md:w-8/12 z-10" src="https://i.postimg.cc/SRtd55rp/jockson.png" alt="me" />
+                <img className="md:w-8/12 z-10" src="https://i.postimg.cc/vmb3zgsM/jockson.webp" alt="me" />
             </div>
             <div className="animate-fade-up animate-delay-500 flex flex-col space-y-20 md:w-6/12 md:justify-between">
                 <div className="flex items-center md:items-start flex-col">
                     <h2 className="font-bold text-5xl bg-gradient-to-r from-purple-900 via-purple-900 via-purple-600 to-purple-900 text-transparent bg-clip-text">Full Stack</h2>
                     <h3 className="pt-2 font-semibold">Web, Mobile e Games</h3>
                     <ul className="flex space-x-5 pt-6">
-                        <li><a className="hover:text-purple-800" href="https://github.com/jockaplay?tab=repositories">Github</a></li>
-                        <li><a className="hover:text-purple-800" href="https://br.linkedin.com/in/jocka-dev?trk=people-guest_people_search-card">Linkedin</a></li>
-                        <li><a className="hover:text-purple-800" href="https://www.instagram.com/j.mt1s/">Instagram</a></li>
+                        <li><a className="hover:text-purple-800" href="https://github.com/jockaplay?tab=repositories"><FontAwesomeIcon icon={faGithub} /></a></li>
+                        <li><a className="hover:text-purple-800" href="https://br.linkedin.com/in/jocka-dev?trk=people-guest_people_search-card"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                        <li><a className="hover:text-purple-800" href="https://www.instagram.com/j.mt1s/"><FontAwesomeIcon icon={faInstagram} /></a></li>
                     </ul>
                 </div>
                 <div> 
